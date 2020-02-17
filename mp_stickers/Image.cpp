9ab51d45ce0453b3bcf05105a963a443c2d4b360
Aidana Karipbayeva <aidana2@illinois.edu>
2020-02-16 19:42:25 -0600

    without scale

---
+++ mp_stickers/Image.cpp
@@ -0,0 +1,170 @@
+#include "Image.h"
+
+
+
+void Image::lighten(){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.l = pixel.l + 0.1;
+      if (pixel.l > 1){
+        pixel.l = 1;
+      }
+    }
+  }
+}
+
+void Image::lighten(double amount){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.l = pixel.l + amount;
+      if (pixel.l > 1){
+        pixel.l = 1;
+      }
+    }
+  }
+
+
+}
+void Image::darken(){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.l = pixel.l - 0.1;
+      if (pixel.l < 0){
+        pixel.l = 0;
+      }
+    }
+  }
+}
+void Image::darken(double amount){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.l = pixel.l - amount;
+      if (pixel.l < 0){
+        pixel.l = 0;
+      }
+    }
+  }
+
+}
+void Image::saturate(){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.s = pixel.s + 0.1;
+      if (pixel.s > 1){
+        pixel.s = 1;
+      }
+    }
+  }
+}
+void Image::saturate(double amount){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.s = pixel.s + amount;
+      if (pixel.s > 1){
+        pixel.s = 1;
+      }
+    }
+  }
+
+
+}
+
+void Image::desaturate(){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.s = pixel.s - 0.1;
+      if (pixel.s < 0){
+        pixel.s = 0;
+      }
+    }
+  }
+}
+void Image::desaturate(double amount){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.s = pixel.s - amount;
+      if (pixel.s < 0){
+        pixel.s = 0;
+      }
+    }
+  }
+}
+
+void Image::grayscale(){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.s = 0;
+    }
+  }
+}
+
+void Image::rotateColor(double degrees){
+  int width = this->width();
+  int height = this->height();
+  for ( int x = 0; x < width; x++) {
+    for ( int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+      pixel.h = pixel.h + degrees;
+      if (pixel.h > 360){
+        pixel.h = 1;
+      }
+    }
+  }
+
+
+
+
+}
+void Image::illinify(){
+  int width = this->width();
+  int height = this->height();
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      HSLAPixel & pixel = this->getPixel(x, y);
+
+      if (pixel.h > 138.5 && pixel.h < 293.5){
+        pixel.h = 216;
+      }
+      else{
+        pixel.h = 11;
+      }
+    }
+  }
+
+
+}
+
+void scale(double factor){
+
+
+
+
+}
+void scale(unsigned w, unsigned h);
--- mp_stickers/Image.h
+++ mp_stickers/Image.h
@@ -5,3 +5,31 @@
 
 #pragma once
 
+#include <string>
+using std::string;
+using namespace std;
+#include "cs225/PNG.h"
+#include "cs225/HSLAPixel.h"
+
+using cs225::HSLAPixel;
+using cs225::PNG;
+
+class Image : public cs225::PNG
+{
+  public:
+    void lighten();
+    void lighten(double amount);
+    void darken();
+    void darken(double amount);
+    void saturate();
+    void saturate(double amount);
+
+    void desaturate();
+    void desaturate(double amount);
+
+    void grayscale();
+    void rotateColor(double degrees);
+    void illinify();
+    void scale(double factor);
+    void scale(unsigned w, unsigned h);
+  };
